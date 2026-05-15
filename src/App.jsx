cfb4a929a3c1b36edb3598b83e073ee0f02d56d4
AmirAli Roghani <AmirAliroqani@gmail.com>
2026-05-15 23:49:52 +0330

    trying to add loading spinner to project

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
-
+import React, { useEffect, useEffectEvent, useState } from "react";
+import "./App.css";
+import { BarLoader, PropagateLoader } from "react-spinners";
 const App = () => {
   return (
-    <div>
-      hallo
-    </div>
-  )
-}
+    <>
+      <div>
+        <h1>Agent Forge</h1>
+      </div>
+    </>
+  );
+};
 
-export default App
+export default App;
